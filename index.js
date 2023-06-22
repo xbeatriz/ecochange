ad8c639d1b46d7d67dd258d46dae1297dc974f12
Beatriz <120328207+xbeatriz@users.noreply.github.com>
2023-06-22 11:15:54 +0100

    MANIFETSO

--- index.js
+++ index.js
@@ -146,17 +146,17 @@ fileInput.addEventListener("change", async (event) => {
 
 //#region SERVICE WORKER
 
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker
-//     .register("./sw.js")
-//     .then((registration) => {
-//       console.log("Service Worker Registered!");
-//       console.log(registration);
-//     })
-//     .catch((error) => {
-//       console.log("SW registration failed!");
-//       console.log(error);
-//     });
-// }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./sw.js")
+    .then((registration) => {
+      console.log("Service Worker Registered!");
+      console.log(registration);
+    })
+    .catch((error) => {
+      console.log("SW registration failed!");
+      console.log(error);
+    });
+}
 
 //#endregion
